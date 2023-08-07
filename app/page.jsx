fc13cd761c7ade4117610e55e66ac41225ad42c6
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-08-07 11:38:49 +0000

    footer added

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,12 @@
+import Footer from "@/components/Footer";
 import FormBuilder from "@/components/FormBuilder";
 export default function Home() {
   const questionArray = [{ quesNo: 1 }];
   const mcqStyle = `rounded border border-gray-300`;
-  return <FormBuilder />;
+  return (
+    <>
+      <FormBuilder />
+      <Footer />
+    </>
+  );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="flex relative bottom-0 left-0 right-0">
+      <div>Add Question</div>
+      <div>Save</div>
+    </div>
+  );
+};
+
+export default Footer;
